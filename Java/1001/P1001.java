26f104910fa959c072993286803f95d4dd5ac682
yna2131 <jn7924@gmail.com>
2023-10-24 12:47:53 -0600

    feat(Java): Solve problem 1001 in Mathematics

---
+++ Java/1001/P1001.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class P1001 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String string = bufferedReader.readLine();
+        StringTokenizer stringTokenizer = new StringTokenizer(string, " ");
+
+        int a = Integer.parseInt(stringTokenizer.nextToken());
+        int b = Integer.parseInt(stringTokenizer.nextToken());
+
+        System.out.println(a - b);
+    }
+}
