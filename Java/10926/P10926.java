a3dfbfbffb1bf1c1fb3aea9a36235d902e48f40a
yna2131 <jn7924@gmail.com>
2025-02-12 16:08:49 -0600

    Solve P19026 using Java

---
+++ Java/10926/P10926.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class P10926 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String str = in.nextLine();
+        System.out.println(str + "??!");
+
+        in.close();
+    }
+}
